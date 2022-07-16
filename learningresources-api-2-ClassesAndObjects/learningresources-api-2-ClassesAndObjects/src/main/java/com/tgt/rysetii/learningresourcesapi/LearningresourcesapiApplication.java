253ce7a09fbbbca665506c38a82c9b6ade18a1d1
SushmaCR <sushmachilumuthurirasinti99@gmail.com>
2022-07-16 16:46:23 +0530

    Classes and Objects code

---
+++ learningresources-api-2-ClassesAndObjects/learningresources-api-2-ClassesAndObjects/src/main/java/com/tgt/rysetii/learningresourcesapi/LearningresourcesapiApplication.java
@@ -0,0 +1,13 @@
+package com.tgt.rysetii.learningresourcesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningresourcesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningresourcesapiApplication.class, args);
+	}
+
+}
