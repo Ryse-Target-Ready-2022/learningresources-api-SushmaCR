6dd7c0b715987c574a6d3e5cb45ac9254accec6c
SushmaCR <81422719+SushmaCR@users.noreply.github.com>
2022-07-16 19:58:30 +0530

    Revert "3 methods collections"

--- learningresources-api-2-ClassesAndObjects/learningresources-api-2-ClassesAndObjects/src/main/java/com/tgt/rysetii/learningresourcesapi/LearningresourcesapiApplication.java
+++ learningresources-api-2-ClassesAndObjects/learningresources-api-2-ClassesAndObjects/src/main/java/com/tgt/rysetii/learningresourcesapi/LearningresourcesapiApplication.java
@@ -3,16 +3,11 @@ package com.tgt.rysetii.learningresourcesapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.tgt.rysetii.learningresourcesapi.service.LearningResourceService;
-
-
 @SpringBootApplication
 public class LearningresourcesapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearningresourcesapiApplication.class, args);
-		LearningResourceService obj = new LearningResourceService();
-		System.out.println(obj.getLearningResource());
 	}
 
 }
